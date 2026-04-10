b9c090b2045261439b40b523a70ff42f41a10183
DerpieDoxie <194111421+DerpieDoxie@users.noreply.github.com>
2026-04-10 12:06:39 +0200

    Add Spring Boot project skeleton

---
+++ colourgen-project/src/main/java/de/dhbwravensburg/webeng/colourgenproject/ColourgenProjectApplication.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburg.webeng.colourgenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColourgenProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColourgenProjectApplication.class, args);
+    }
+
+}
